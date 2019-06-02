a1563fce14409b1e2ec2666a5eb3cd6e27739324
acuD1 <arsciand@student.42.fr>
2019-06-02 13:58:16 +0200

    Up no leaks now

--- srcs/get_next_line.c
+++ srcs/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: arsciand <arsciand@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/23 18:50:58 by arsciand          #+#    #+#             */
-/*   Updated: 2019/06/01 17:33:58 by arsciand         ###   ########.fr       */
+/*   Updated: 2019/06/02 13:57:56 by arsciand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,44 +25,37 @@ static int	check_line(char **stack)
 	return (1);
 }
 
-static int	init(int const fd, char **line, char *buff, char **stack)
+static int	init(int const fd, char **line, char **stack)
 {
 	if (fd == -1 || FD_MAXSET > 4864 || BUFF_SIZE < 1 || !line
-			|| (read(fd, buff, 0) == -1)
 			|| (!(stack[fd]) && !(stack[fd] = ft_strnew(0))))
-	{
-		ft_strdel(&buff);
 		return (0);
-	}
 	return (1);
 }
 
 int			get_next_line(int const fd, char **line)
 {
 	static char	*stack[FD_MAXSET];
-	char		*buff;
+	char		buff[BUFF_SIZE + 1];
 	char		*tmp;
 	int			ret;
 
-	if (!(buff = ft_memalloc(BUFF_SIZE + 1)) || (!init(fd, line, buff, stack)))
+	if ((read(fd, buff, 0) == -1) || !(init(fd, line, stack)))
 		return (-1);
 	while (!(check_line(&stack[fd])) && (ret = read(fd, buff, BUFF_SIZE)))
 	{
 		buff[ret] = '\0';
 		tmp = stack[fd];
-		stack[fd] = ft_strjoin(tmp, buff);
-		free(buff);
+		stack[fd] = ft_strjoinf(tmp, buff, 1);
 	}
-	ft_strdel(&buff);
+	ft_bzero(buff, BUFF_SIZE);
 	*line = ft_strsub(stack[fd], 0, ft_strclen(stack[fd], '\n'));
 	if (*stack[fd])
 	{
 		if (check_line(&stack[fd]))
 			ft_strcpy(stack[fd], ft_strchr(stack[fd], '\n') + 1);
-		else
-			ft_strdel(&stack[fd]);
+		ft_strdel(&stack[fd]);
 		return (1);
 	}
-	ft_strdel(&stack[fd]);
 	return (0);
 }
